d0a6b0f6327c42ff2ab86bef0ce144dcbc350cf7
kittkat7 <122490347+KittKat7@users.noreply.github.com>
2024-03-30 17:11:22 -0400

    Fixed inconsistancy between spaces and tabs in PlayerObject

--- src/main/java/edu/appstate/cs/projectname/PlayerObject.java
+++ src/main/java/edu/appstate/cs/projectname/PlayerObject.java
@@ -7,66 +7,66 @@ import java.awt.image.BufferedImage;
  */
 public class PlayerObject extends GameObject {
 
-    // Players current x & y position.
-    private int x;
-    private int y;
+	// Players current x & y position.
+	private int x;
+	private int y;
 
-    // Reference to the game board.
-    private Board board; 
+	// Reference to the game board.
+	private Board board; 
 
-    /**
+	/**
 	 * Constructor for playerObject
-     * 
-     * @param board The game board for the player.
-     * @param StartX The initial x-coordinate of the player.
-     * @param StartY the initial y-coordinate of the player.
+	 * 
+	 * @param board The game board for the player.
+	 * @param StartX The initial x-coordinate of the player.
+	 * @param StartY the initial y-coordinate of the player.
 	 */
 	public PlayerObject(Board board, int StartX, int StartY){
-        super(false);
+		super(false);
 		super.image = this.getImage();
-        this.board = board;
-        this.x = StartX;
-        this.y = StartY;
+		this.board = board;
+		this.x = StartX;
+		this.y = StartY;
 	}
 
-    /**
+	/**
 	 * Moves the player upwards on the game board. 
 	 */
 	public void moveUp() {
 		if (y > 0 && board.canMove(x, y - 1))
-        {
-            y--;
-        }
+		{
+			y--;
+		}
 	}
 
-    /**
+	/**
 	 * Moves the player down on the game board. 
 	 */
 	public void moveDown() {
 		if (y < board.getHeight() - 1 && board.canMove(x, y + 1))
-        {
-            y++;
-        }
+		{
+			y++;
+		}
 	}
 
-    /**
+	/**
 	 * Moves the player left on the game board. 
 	 */
 	public void moveLeft() {
 		if (x > 0 && board.canMove(x - 1, y)) 
-        {
-            x--;
-        }
+		{
+			x--;
+		}
 	}
 
-    /**
+	/**
 	 * Moves the player right on the game board. 
 	 */
 	public void moveRight() {
 		if (x < board.getWidth() - 1 && board.canMove(x + 1, y))
-        {
-            x++;
-        }
+		{
+			x++;
+		}
 	}
 
 	/**
