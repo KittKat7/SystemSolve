61b3b0e2ef3ae4a8e7b7384279a6a773ccb24083
kittkat7 <122490347+KittKat7@users.noreply.github.com>
2024-04-04 13:09:43 -0400

    Fixed eddor in ReadInstructions.java

--- src/main/java/edu/appstate/cs/projectname/ReadInstructions.java
+++ src/main/java/edu/appstate/cs/projectname/ReadInstructions.java
@@ -3,29 +3,34 @@ package edu.appstate.cs.projectname;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Class to get and read the text file containing the level instructions.
  */
 public class ReadInstructions {
 
-    //String that contains the repeated parts of the path
-    private static String filePathStr = ".//src//main//java//edu//appstate//cs//projectname//Instructions";
-    
-    /**
-     * Method that takes the level as an int and
-     * loads the corresponding text file then
-     * returns it as a string
-     * @param level level the player is on as an int
-     * @return returns string read from file
-     */
-    public static String readFile(int level)
-    {
-        try {
-            return Files.readString(Paths.get(filePathStr + "//level" + level + "Instr.txt"));
-        } catch (IOException e) {
-            return "File Not Found";
-        }
-    }
+	// String that contains the repeated parts of the path
+	private static String filePathStr = ".//src//main//java//edu//appstate//cs//projectname//Instructions";
+
+	/**
+	 * Method that takes the level as an int and
+	 * loads the corresponding text file then
+	 * returns it as a string
+	 * 
+	 * @param level level the player is on as an int
+	 * @return returns string read from file
+	 */
+	public static String readFile(int level) {
+		try {
+			String output = "";
+			List<String> lines = Files.readAllLines(Paths.get(filePathStr + "//level" + level + "Instr.txt"));
+			for (String str : lines)
+				output += str + "\n";
+			return output;
+		} catch (IOException e) {
+			return "File Not Found";
+		}
+	}
 
 }
