c2d0cda0a0e5ae30bf3add151446bd6c2f848a61
kittkat7 <122490347+KittKat7@users.noreply.github.com>
2024-04-04 22:45:12 -0400

    Moved assets to resources folder per maven docs

--- src/main/java/edu/appstate/cs/projectname/GameObject.java
+++ src/main/java/edu/appstate/cs/projectname/GameObject.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;;
+
 /**
  * An object which represents the objects/characters/entities in the game.
  */
@@ -13,7 +14,7 @@ public abstract class GameObject {
 	// Whether or not this object is an obstacle, and will block movement.
 	protected boolean isObstacle;
 
-	//BufferedImage object to store image objects
+	// BufferedImage object to store image objects
 	protected BufferedImage image;
 
 	/**
@@ -23,35 +24,35 @@ public abstract class GameObject {
 	 */
 	GameObject(boolean isObstacle) {
 		this.isObstacle = isObstacle;
-		
-	}//GameObject
 
-	GameObject()
-	{
+	}// GameObject
+
+	GameObject() {
 		this.isObstacle = false;
 	}
 
 	/**
-	 * Returns the isObstacle flag. If isObstacle, the player cannot traverse this area.
+	 * Returns the isObstacle flag. If isObstacle, the player cannot traverse this
+	 * area.
 	 * 
 	 * @return A bool flag marking whether this object can be traversed.
 	 */
 	public boolean getIsObstacle() {
 		return isObstacle;
-	}//getIsObstacle
+	}// getIsObstacle
 
 	/**
-	 * Overrides GameObject get image. 
+	 * Overrides GameObject get image.
 	 * Reads png file and returns the image
+	 * 
 	 * @return returns image read for goal object
 	 */
 	public static BufferedImage getImage(String fileName) {
 		try {
-			File wall = new File
-				(".//src//main//java//edu//appstate//cs//projectname//BackGroundImages//" + fileName);
+			File wall = new File("./src/main/resources/edu/appstate/cs/projectname/images/" + fileName);
 			return ImageIO.read(wall);
 		} catch (IOException e) {
 			return null;
-		} 
+		}
 	}
-}//GameObject
+}// GameObject
