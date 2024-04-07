aa8c3a91535c562de3fe0c4708c1195517da306d
kittkat7 <122490347+KittKat7@users.noreply.github.com>
2024-04-07 13:33:45 -0400

    added protected keywords to Interpreter

--- src/main/java/edu/appstate/cs/projectname/Interpreter.java
+++ src/main/java/edu/appstate/cs/projectname/Interpreter.java
@@ -1,6 +1,12 @@
 package edu.appstate.cs.projectname;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Interpreter {
+	private static final HashSet<String> KEYWORDS = new HashSet<String>(Arrays.asList(
+			"var", "while", "moveUp", "moveDown", "moveLeft", "moveRight"));
 	private static Interpreter interpreter;
 	private PlayerObject player;
 
@@ -16,14 +22,18 @@ public class Interpreter {
 		return interpreter;
 	}// getInterpreter()
 
+	HashMap<String, Integer> vars = new HashMap<String, Integer>();
+
+	HashMap<String, Integer> vars = new HashMap<String, Integer>();
+
 	/**
-     * Set the player object for the interpreter.
-     * 
-     * @param player The player object to set.
-     */
-    public void setPlayer(PlayerObject player) {
-        this.player = player;
-    }
+	 * Set the player object for the interpreter.
+	 * 
+	 * @param player The player object to set.
+	 */
+	public void setPlayer(PlayerObject player) {
+		this.player = player;
+	}
 
 	/**
 	 * Parses the string param, and returns a string with the completion/error
@@ -35,8 +45,13 @@ public class Interpreter {
 	 */
 	public void parse(String str) {
 		String[] lines = str.split("\n");
-		for (String line : lines) {
-			parseLine(line);
+		try {
+			for (String line : lines) {
+				parseLine(line);
+			}
+		} catch (InterpretingException e) {
+			System.out.println("An exception in the code was encountered.");
+			System.out.println(e.getMessage());
 		}
 	}// parse(String)
 
@@ -47,16 +62,41 @@ public class Interpreter {
 	 * @param line The command to parse
 	 * @return Either an empty string, or an error message
 	 */
-	private String parseLine(String line) {
-		if (line == "Up")
-			player.move("up");
-		else if (line == "Left")
-			player.move("left");
-		else if (line == "Right")
-			player.move("right");
-		else if (line == "Down")
-			player.move("down");
-		return "";
+	private void parseLine(String line) throws InterpretingException {
+		String[] parts = line.split(" ");
+		// Variable detetion
+		if (parts[0] == "var") {
+			if (KEYWORDS.contains(parts[1]))
+				throw new InterpretingException("Trying to use keyword as variable name '" + parts[1] + "'");
+			else if (vars.keySet().contains(parts[1]))
+				throw new InterpretingException("Variable " + parts[1]);
+			else
+				vars.put(parts[1], Integer.parseInt(parts[2]));
+		} else if (vars.keySet().contains(parts[0]))
+			; // TODO: modifying var
+		else if (line == "moveUp")
+			; // TODO: move up
+		else if (line == "moveLeft")
+			; // TODO: move left
+		else if (line == "moveRight")
+			; // TODO: move right
+		else if (line == "moveDown")
+			; // TODO: move down
+		else
+			throw new InterpretingException("Unknown command '" + line + "'");
 	}// parseLine()
 
 }// Interpreter
+
+class InterpretingException extends Exception {
+	String messageString;
+
+	InterpretingException() {
+		this("");
+	}
+
+	InterpretingException(String msg) {
+		super(msg);
+		messageString = msg;
+	}
+}
