59a12be88d87dc8e85ce7d59cd3d8d59bb86ed3c
kittkat7 <122490347+KittKat7@users.noreply.github.com>
2024-04-12 00:05:17 -0400

    Added Level class

---
+++ src/main/java/edu/appstate/cs/projectname/Level.java
@@ -0,0 +1,26 @@
+package edu.appstate.cs.projectname;
+
+import java.io.IOException;
+
+public class Level {
+
+	private int index;
+	private Board board;
+	private Interpreter interpreter;
+	private PlayerObject player;
+
+	/**
+	 * Constructs a Level instance based on the provided level index. Loads the
+	 * board, and initializes the player and interpreter fields.
+	 * 
+	 * @param index
+	 * @throws IOException
+	 */
+	public Level(int index) throws IOException { // TODO: Handle IOException
+		this.index = index;
+		board = Board.createBoardFromFile("level" + index);
+		player = board.getPlayer();
+		interpreter = new Interpreter(player);
+	}
+
+}
