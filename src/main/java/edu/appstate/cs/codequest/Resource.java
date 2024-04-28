434f22cf97a0090d363342a48af421ea32650007
KittKat <122490347+KittKat7@users.noreply.github.com>
2024-04-28 00:03:41 -0400

    Added test for ReadInstructions

--- src/main/java/edu/appstate/cs/codequest/Resource.java
+++ src/main/java/edu/appstate/cs/codequest/Resource.java
@@ -1,5 +1,5 @@
 package edu.appstate.cs.codequest;
 
 public class Resource {
-	public static String resourcePath = "./src/main/resources/edu/appstate/cs/codequest/";
+	public static final String resourcePath = "./src/main/resources/edu/appstate/cs/codequest/";
 }
