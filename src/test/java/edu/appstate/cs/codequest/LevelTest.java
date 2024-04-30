473e9307e19a6f8e603105a5c578d4bf06bf3c8f
KittKat <122490347+KittKat7@users.noreply.github.com>
2024-04-30 18:56:25 -0400

    Level is now a singleton

--- src/test/java/edu/appstate/cs/codequest/LevelTest.java
+++ src/test/java/edu/appstate/cs/codequest/LevelTest.java
@@ -9,66 +9,66 @@ import org.junit.jupiter.api.Test;
 public class LevelTest {
 
     @Test
-    public void TestGetIsAtGoalLevel0() throws IOException{
-        assertTrue(new Level(0).getIsAtGoal());
+    public void TestGetIsAtGoalLevel0() throws IOException {
+        assertTrue(Level.newLevel(0).getIsAtGoal());
     }
 
     @Test
-    public void TestGetIsAtGoalLevel1() throws IOException{
-        assertTrue(!(new Level(1).getIsAtGoal()));
+    public void TestGetIsAtGoalLevel1() throws IOException {
+        assertTrue(!(Level.newLevel(1).getIsAtGoal()));
     }
 
     @Test
-    public void TestGetIsAtGoalLevel2() throws IOException{
-        assertTrue(!(new Level(2).getIsAtGoal()));
+    public void TestGetIsAtGoalLevel2() throws IOException {
+        assertTrue(!(Level.newLevel(2).getIsAtGoal()));
     }
 
     @Test
-    public void TestGetIsAtGoalLevel3() throws IOException{
-        assertTrue(!(new Level(3).getIsAtGoal()));
+    public void TestGetIsAtGoalLevel3() throws IOException {
+        assertTrue(!(Level.newLevel(3).getIsAtGoal()));
     }
 
     @Test
-    public void TestGetIsAtGoalLevel4() throws IOException{
-        assertTrue(!(new Level(3).getIsAtGoal()));
+    public void TestGetIsAtGoalLevel4() throws IOException {
+        assertTrue(!(Level.newLevel(3).getIsAtGoal()));
     }
 
     @Test
-    public void TestGetIsAtGoalLevel5() throws IOException{
-        assertTrue(!(new Level(5).getIsAtGoal()));
+    public void TestGetIsAtGoalLevel5() throws IOException {
+        assertTrue(!(Level.newLevel(5).getIsAtGoal()));
     }
 
     @Test
-    public void testLevel1BoardHasGoal() throws IOException{
-        assertTrue(new Level(1).getBoard().getHasGoal());
+    public void testLevel1BoardHasGoal() throws IOException {
+        assertTrue(Level.newLevel(1).getBoard().getHasGoal());
     }
 
     @Test
-    public void testLevel2BoardHasGoal() throws IOException{
-        assertTrue(new Level(2).getBoard().getHasGoal());
+    public void testLevel2BoardHasGoal() throws IOException {
+        assertTrue(Level.newLevel(2).getBoard().getHasGoal());
     }
 
     @Test
-    public void testLevel3BoardHasGoal() throws IOException{
-        assertTrue(new Level(3).getBoard().getHasGoal());
+    public void testLevel3BoardHasGoal() throws IOException {
+        assertTrue(Level.newLevel(3).getBoard().getHasGoal());
     }
 
     @Test
-    public void testLevel4BoardHasGoal() throws IOException{
-        assertTrue(new Level(4).getBoard().getHasGoal());
+    public void testLevel4BoardHasGoal() throws IOException {
+        assertTrue(Level.newLevel(4).getBoard().getHasGoal());
     }
 
     @Test
-    public void testLevel5BoardHasGoal() throws IOException{
-        assertTrue(new Level(5).getBoard().getHasGoal());
+    public void testLevel5BoardHasGoal() throws IOException {
+        assertTrue(Level.newLevel(5).getBoard().getHasGoal());
     }
 
     @Test
-    public void testRunCode() throws IOException{
-        Level l = new Level(0);
+    public void testRunCode() throws IOException {
+        Level l = Level.newLevel(0);
         int x = l.getPlayerObject().getPlayerX();
         int y = l.getPlayerObject().getPlayerY();
         l.runCode("moveU");
-        assertTrue(((l.getPlayerObject().getPlayerX() == x) && (l.getPlayerObject().getPlayerY() == y-1)));
+        assertTrue(((l.getPlayerObject().getPlayerX() == x) && (l.getPlayerObject().getPlayerY() == y - 1)));
     }
 }
